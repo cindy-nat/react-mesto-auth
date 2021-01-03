59fb814618d39140676a21c5d5581f4c035b17b1
Natalia Popova <cindy.n-popova@yandex.ru>
2021-01-03 23:34:11 +0300

    layout for login is done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Login () {
   return (
-    <form>
-      <p>Popo</p>
-      <input />
+    <form className="form">
+      <h2 className="form__title">Вход</h2>
+      <input className="form__input" placeholder="Email" type="email"/>
+      <input className="form__input" placeholder="Пароль" type="password"/>
+      <button type="submit" className="form__submit-button">Войти</button>
     </form>
   )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -5,8 +5,8 @@ function Register () {
   return (
     <form className="form">
       <h2 className="form__title">Регистрация</h2>
-      <input className="form__input" placeholder="Email"/>
-      <input className="form__input" placeholder="Пароль"/>
+      <input className="form__input" placeholder="Email" type="email"/>
+      <input className="form__input" placeholder="Пароль" type="password"/>
       <button type="submit" className="form__submit-button">Зарегестрироваться</button>
       <p className="form__text">Уже зарегистрированы? <Link to="/sign-in" className="form__link">Войти</Link></p>
     </form>
