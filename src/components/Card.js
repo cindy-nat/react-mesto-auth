6c52e939b6f98887e1d0149d4674c111c2d92d38
Natalia Popova <cindy.n-popova@yandex.ru>
2021-01-06 17:38:36 +0300

    removed if for showing likes in the card. Now 0 is shown

--- src/components/Card.js
+++ src/components/Card.js
@@ -35,7 +35,7 @@ function Card({onCardClick, onCardLike, onCardDelete, card}) {
           <h2 className="cards__name">{card.name}</h2>
           <div>
             <button type="button" className={`cards__like ${cardLikeButtonClassName}`} onClick={handleLikeClick}></button>
-            <p className="cards__like-number">{card.likes.length>0 && card.likes.length}</p>
+            <p className="cards__like-number">{card.likes.length}</p>
           </div>
         </div>
       </div>
